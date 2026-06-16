5c1e4f20ced67ff8fa596093746e93901745ff2a
QSoraQ <284651@student.pwr.edu.pl>
2026-06-17 00:39:01 +0200

    koncowa wersja Magda

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 /**
  ******************************************************************************
- * @file  Project/main.c 
- * @author MCD Application Team
+ * @file    Project/main.c 
+ * @author  MCD Application Team
  * @version V2.3.0
- * @date  16-June-2017
- * @brief  Main program body
-  ******************************************************************************
+ * @date    16-June-2017
+ * @brief   Main program body
+ ******************************************************************************
  * @attention
  *
  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
@@ -14,7 +14,7 @@
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
  *
- *    http://www.st.com/software_license_agreement_liberty_v2
+ * http://www.st.com/software_license_agreement_liberty_v2
  *
  * Unless required by applicable law or agreed to in writing, software 
  * distributed under the License is distributed on an "AS IS" BASIS, 
@@ -25,32 +25,24 @@
  ******************************************************************************
  */ 
 
-	
-	
-	//67676767676767676776
-//69696969669696
-//69696969669696
-
-
 /* Includes ------------------------------------------------------------------*/
 #include "stm8s.h"
-#include <stdbool.h>
+#include <stdbool.h>//biblioteka dodajaca bool
 
 //Magda: UART
 #define TX_SIZE 96
 #define RX_SIZE 24//na GG:MM:SS DD.MM.RRRR
 /* Private defines -----------------------------------------------------------*/
 //Magda: zmienne do sterowania UART
-//Koszyczki na dane uwu tablice 
 int8_t rx_buff[RX_SIZE];//miejsce/bufor na znaki z terminala
-uint8_t tx_buff[TX_SIZE];//bufor dla tekstu wyslanego przez mikro. do komp
+uint8_t tx_buff[TX_SIZE];//bufor dla tekstu wyslanego przez mikro. do komp/ dla wipisywania b³êdów
 uint8_t rx_comand[RX_SIZE];//tablica pomocnicza, przechowuje komede na czas jej roszyfrowania
-uint8_t tx_wr;//Wska nik zapisu do bufora nadawczego (gdzie dopisa now liter do wys ania)
-uint8_t tx_rd;//Wska nik odczytu z bufora nadawczego (kt r liter uk ad UART ju wys a )
-uint8_t rx_wr;// Wska nik zapisu bufora odbiorczego (do ktorego bufora w o y kolejny odebrany znak)
-volatile uint8_t CarRet = 0; // Informacja o odebraniu znaku '\r' (Enter)
-uint8_t ind;//Zmienna pomocnicza (indeks) u ywana w p tlach do czyszczenia lub kopiowania tablic
-// Magda: Flaga zegara - na starcie false (zegar stoi, dop ki UART nie wy le czasu)
+uint8_t tx_wr;//Wskaznik zapisu do bufora nadawczego (gdzie dopisa nowa liter do wyslania)
+uint8_t tx_rd;//Wskaznik odczytu z bufora nadawczego (ktora liter uklad UART juz wyslania )
+uint8_t rx_wr;// Wskaznik zapisu bufora odbiorczego (w ktor¹ pozycje tabeli wlozyc kolejny znak)
+volatile uint8_t CarRet = 0; // Flaga Entera
+uint8_t ind;//Zmienna pomocnicza (indeks) uzywana w petlach do czyszczenia bufora odbiorczego
+// Magda: Flaga zegara -na starcie false (zegar stoi, dopoki UART nie dostanie poprawnego czasu)
 volatile bool zegar_zyje = false;
 
 volatile unsigned int tick_ms = 0;
@@ -71,7 +63,7 @@ unsigned int cyfra1 = 0;
 unsigned int cyfra2 = 0;
 unsigned int cyfra3 = 0;
 unsigned int clock_state = 0;
-unsigned int krok_diod = 0;//Magda:
+unsigned int krok_diod = 0;//Magda: Licznik krokow sekwencji diod, steruje zmianom diud.
 volatile unsigned int manual_clock_state = 0;
 unsigned int current_display_state;
 int intigers[4] = {0,0,0,0}; //Wpisujecie ta liste jako drugi element funkcji convertNumber.
@@ -108,8 +100,8 @@ void segm_shift(char shift)
     GPIO_WriteHigh(GPIOD,GPIO_PIN_3); //SDI=1
    else
     GPIO_WriteLow(GPIOD,GPIO_PIN_3); //SDI=0
-	  GPIO_WriteHigh(GPIOC,GPIO_PIN_3);
-	  GPIO_WriteLow(GPIOC,GPIO_PIN_3);
+	 GPIO_WriteHigh(GPIOC,GPIO_PIN_3);
+	 GPIO_WriteLow(GPIOC,GPIO_PIN_3);
 
 			
   shift >>= 1;
@@ -126,7 +118,6 @@ void segm_latch(char pos, char val)
 }
 //Hubert
 //funkcje pomocnicze
-//Funkcja do konwersji liczb np. 113 na elementy w tablicy np. intigers = {0 , 1, 1 ,3}
 void convertNumber(int number, int numbersArray[]){
 	if(number >9999){
 		number = 9999;
@@ -137,94 +128,95 @@ void convertNumber(int number, int numbersArray[]){
 	numbersArray[3] = number%10;
 
 }
-//Funkcja do odswiezania ekranu za pomoca pobranych wartoci z tablicy intigers!!!
 void refreshSegm(void){//odswierza ekran 
 	segm_latch(0, segm_dec[intigers[0]]);
 	segm_latch(1, segm_dec[intigers[1]]);
 	segm_latch(2, segm_dec[intigers[2]]);
 	segm_latch(3, segm_dec[intigers[3]]);
 }
-//Magda: 
-//Funkcje podwalone od Profesora Jerzaka ;3
-//Funkcja od dodawania znakow do nadania
-void tx_put(uint8_t c)//Funkcja dodaje pojedynczy znak do bufora nadawczego
+//Magda: Funkcja wklada jeden znak do bufora nadawczego i odpala przerwanie UART
+void tx_put(uint8_t c)
 {
- tx_buff[tx_wr] = c;// Zapisz znak do bufora na pozycj wskazywan przez tx_wr
- if (++tx_wr >= TX_SIZE) tx_wr = 0;// Zwi ksz indeks zapisu; je li osi gnie koniec bufora, wr  na 0
- UART1_ITConfig(UART1_IT_TXE, ENABLE); // W czenie przerwa nadajnika
+ tx_buff[tx_wr] = c;// Zapisz znak do bufora na pozycji wskaznika tx_wr
+ if (++tx_wr >= TX_SIZE) tx_wr = 0;// Przesun wskaznik zapisu. Jesli koniec tablicy, wroc na poczatek
+ UART1_ITConfig(UART1_IT_TXE, ENABLE);//Wlacz przerwanie od pustego rejestru nadawczego (rozpocznij wysylanie)  
 }
-//Funkcja od przeslania przez kabel
-uint8_t tx_get(void)// Funkcja pobiera kolejny znak z bufora nadawczego w celu jego wys ania przez kabel
+uint8_t tx_get(void)//unkcja pobiera jeden znak z bufora nadawczego (uzywana w przerwaniu)
 {
- uint8_t c = tx_buff[tx_rd];// Odczytaj znak z bufora z pozycji wskazywanej przez tx_rd
- if (++tx_rd >= TX_SIZE) tx_rd = 0;
- return c;// Zwr  pobrany znak
+ uint8_t c = tx_buff[tx_rd];// Pobierz znak z bufora z pozycji wskaznika tx_rd
+ if (++tx_rd >= TX_SIZE) tx_rd = 0;// Przesun wskaznik odczytu. Jesli koniec tablicy, wroc na poczatek
+ return c;// Zwroc pobrany znak
 }
-//Funkcja od sprawdzania czy cos czeka na wyslanie
+//Magda: Funkcja sprawdza, czy w buforze nadawczym sa jeszcze jakies znaki do wyslania
 uint8_t tx_cnt(void)
 {
- if (tx_wr != tx_rd)// Zwr  1, je li indeks zapisu r ni si od odczytu (s znaki w buforze)
+ if (tx_wr != tx_rd)// Jesli wskaznik zapisu jest rozny od wskaznika odczytu, to znaczy ze sa dane
   return 1;
- else
-  return 0;// Zwr  0, je li indeksy s r wne (bufor jest pusty)
+ else// Jesli wskazniki sa równe, bufor jest pusty
+  return 0;
 }
-//Funkcja od zebrania informacji od uzytkownika i do wlozenia jej w odpowiedznie miejsce do przechowania
+//Funkcja odbiera pojedynczy znak z terminala i wklada go do bufora odbiorczego
 void rx_put(char c)
 {
     if (c == 13) // Enter
     {
-        CarRet = 1;
+        CarRet = 1;// Wykryto Enter, ustaw flage na 1 (pêtla glowna to zauwazy)
     }
-    else if (c >= ' ' && rx_wr < RX_SIZE) // Zapisuj tylko jeœli to znak (a nie œmieci)
+    else if (c >= ' ' && rx_wr < RX_SIZE)// Jesli znak to zwykla litera/cyfra i jest miejsce w buforze 
     {
-        rx_buff[rx_wr] = c;
-        if (++rx_wr >= RX_SIZE) rx_wr = 0;
+        rx_buff[rx_wr] = c;// Zapisz znak do bufora na pozycji rx_wr
+        if (++rx_wr >= RX_SIZE) rx_wr = 0;// Przesun wskaznik zapisu. Jesli koniec tablicy, wroc na poczatek
     }
 }
-//funkcja do wysylania tekstu: mikro potrafi gadac do kompa
-void send_string(const char* s)//to co wpiszemy rozbija na pojedyncza literke i wsadza do tx_put
+// Magda: Funkcja pozwala wyslac caly napis (ciag znakow) do komputera na raz
+void send_string(const char* s)
 {
- while (*s) tx_put(*s++);
+ while (*s) tx_put(*s++);// Dopoki nie trafimy na koniec napisu, wysylaj znak po znaku przez tx_put
 }
-//.....................................
-//Magda: fukcja co bierze z terminala co wpisalismy i sorktu i dekoduje z ascii/ zajeba am to od germina bo chuj wie jak to zrobic T^T
+
+// Magda: Fukcja dekoduj¹ca z walidacj¹ zakresów
 void dekoduj_czas(void)
 {
-    // GG:MM:SS DD.MM.RRRR
-    hour          = (rx_buff[0]  - '0') * 10 + (rx_buff[1]  - '0');
-    // rx_buff[2] to ':' - pomijamy
-    minute        = (rx_buff[3]  - '0') * 10 + (rx_buff[4]  - '0');
-    // rx_buff[5] to ':' - pomijamy
-    second        = (rx_buff[6]  - '0') * 10 + (rx_buff[7]  - '0');
-    // rx_buff[8] to ' ' (spacja) - pomijamy
-    day           = (rx_buff[9]  - '0') * 10 + (rx_buff[10] - '0');
-    // rx_buff[11] to '.' - pomijamy
-    current_month = (rx_buff[12] - '0') * 10 + (rx_buff[13] - '0');
-    // rx_buff[14] to '.' - pomijamy
-    year          = (rx_buff[15] - '0') * 1000 + (rx_buff[16] - '0') * 100 + (rx_buff[17] - '0') * 10 + (rx_buff[18] - '0');
+    // 1. Dekodujemy znaki ze sztywnych pozycji bufora
+    hour=(rx_buff[0]  - '0') * 10 + (rx_buff[1]  - '0');
+    minute=(rx_buff[3]  - '0') * 10 + (rx_buff[4]  - '0');
+    second=(rx_buff[6]  - '0') * 10 + (rx_buff[7]  - '0');
+    day=(rx_buff[9]  - '0') * 10 + (rx_buff[10] - '0');
+    current_month=(rx_buff[12] - '0') * 10 + (rx_buff[13] - '0');
+    year=(rx_buff[15] - '0') * 1000 + (rx_buff[16] - '0') * 100 + (rx_buff[17] - '0') * 10 + (rx_buff[18] - '0');
 
-    // WALIDACJA
-    if (hour > 23 || minute > 59 || second > 59 || current_month < 1 || current_month > 12) {
-        send_string("BLAD: Zly format lub dane!\r\n");
-    } else {
-        zegar_zyje = true;
-        send_string("Zegarek ustawiony!\r\n");
+    // 2. Jesli zdekodowane liczby przekraczaja normalne zakresy ( smieci z RAMu)zeruje zmienne)
+    // lub liczby przekraczaj¹ normalne zakresy, czyœcimy je, ¿eby nie psuæ okna Watch
+    if (hour > 23 || minute > 59 || second > 59 || current_month < 1 || current_month > 12 || year > 3000) {
+        hour = 0;
+        minute = 0;
+        second = 0;
+        day = 0;
+        current_month = 0;
+        year = 0;
+        send_string("BLAD: Zly format danych. Wpisz ponownie.\r\n");
+        return;
+    }
+
+    // 3. Sprawdzenie dni przy u¿yciu tablicy month
+    if (day < 1 || day > month[current_month - 1]) {//Bierze dane o ilosci dni w miesiacu, jesli sie nie zgada, zeruje
+        hour = 0; minute = 0; second = 0; day = 0; current_month = 0; year = 0; //reset
+        send_string("BLAD: Ten miesiac nie ma tylu dni\r\n");
+    } 
+    // 4. Jeœli format i zakresy s¹ idealne - zegar rusza
+    else {
+        zegar_zyje = true;// Zezwolenie na start sekundnika i sekwencji diod
+        send_string("Zegarek Zyje!\r\n");
     }
 }
+
 // Tymek
-// funkcja do inicjalizacji bazera
 void TIM2_Config(void)
 {
- /* Time base configuration */
- TIM2_TimeBaseInit(TIM2_PRESCALER_2, period); //Konfiruacja podstawy czasu, zmienna period okresla okres sygna u PWM
-
- /* PWM1 Mode configuration: Channel1 */ 
- TIM2_OC1Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE,time, TIM2_OCPOLARITY_HIGH); //Konfiguracja PWM, zmienna time okresla szerokosc impulsu
+ TIM2_TimeBaseInit(TIM2_PRESCALER_2, period); 
+ TIM2_OC1Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE,time, TIM2_OCPOLARITY_HIGH); 
  TIM2_OC1PreloadConfig(ENABLE);
-
  TIM2_ARRPreloadConfig(ENABLE);
-
- /* TIM2 enable counter */
  TIM2_Cmd(ENABLE);
 }
 
@@ -238,21 +230,20 @@ void main(void)
    GPIO_Init(GPIOC, GPIO_PIN_6, GPIO_MODE_OUT_PP_LOW_FAST); // D3
    GPIO_Init(GPIOE, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_FAST); // D4
  
- 
  //Konfig guzików
- 	GPIO_Init(GPIOB, GPIO_PIN_4, GPIO_MODE_IN_FL_IT); //S1
+	GPIO_Init(GPIOB, GPIO_PIN_4, GPIO_MODE_IN_FL_IT); //S1
 	GPIO_Init(GPIOB, GPIO_PIN_3, GPIO_MODE_IN_FL_IT); //S2
 	
 	EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOB, EXTI_SENSITIVITY_RISE_FALL);
 	//Konfig Uart z komunikatem:
-	GPIO_Init(GPIOD, GPIO_PIN_5, GPIO_MODE_OUT_PP_HIGH_FAST); // PD5 TX (Nadajnik)
-  GPIO_Init(GPIOD, GPIO_PIN_6, GPIO_MODE_IN_PU_NO_IT);   // PD6 RX (Odbiornik)
+	GPIO_Init(GPIOD, GPIO_PIN_5, GPIO_MODE_OUT_PP_HIGH_FAST); // PD5 TX
+  GPIO_Init(GPIOD, GPIO_PIN_6, GPIO_MODE_IN_PU_NO_IT);   // PD6 RX
 	UART1_Init((uint32_t)9600, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO, UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE);
   UART1_ITConfig(UART1_IT_RXNE_OR, ENABLE);
  // Config bazera:
  TIM2_Config();
-  GPIO_Init(GPIOC,GPIO_PIN_1,GPIO_MODE_OUT_PP_LOW_FAST);//Konfiguracja GPIO buzzera
-	GPIO_WriteHigh(GPIOC, GPIO_PIN_1); //Wylaczenie buzzera
+  GPIO_Init(GPIOC,GPIO_PIN_1,GPIO_MODE_OUT_PP_LOW_FAST);
+	GPIO_WriteHigh(GPIOC, GPIO_PIN_1); 
 	send_string("Zegarek nie zyje. Ustaw aktualny czas i date (GG:MM:SS DD.MM.RRRR) i kliknij Enter:\r\n");
 	//...........................................
 	TIM4_TimeBaseInit(TIM4_PRESCALER_16,124); //Odliczanie 1ms
@@ -260,29 +251,32 @@ void main(void)
 	TIM4_ITConfig(TIM4_IT_UPDATE, ENABLE);
 	TIM4_Cmd(ENABLE);
 	enableInterrupts();
- while (1)//Hubert: liczenie dni
+ while (1)
  {
-
-  if (CarRet == 1)//Po enterze ON ZYJE MUHAHAHAHAHA
+  if (CarRet == 1)
    {
-     dekoduj_czas(); // Wywo ujemy nasze dekodowanie
+      dekoduj_czas(); 
+			for(ind = 0; ind < RX_SIZE; ind++) //Czyszczenie calego bufora ze starych znakow, aby nie zafalszowac kolejnego wpisu
+      { 
+          rx_buff[ind] = 0; 
+      }
 		 rx_wr = 0;
-     CarRet = 0;   // Zerujemy flag Entera
-     UART1_ITConfig(UART1_IT_RXNE_OR, ENABLE); // Odblokowujem		y UART do dalszego s uchania
+      CarRet = 0;   
+      UART1_ITConfig(UART1_IT_RXNE_OR, ENABLE); 
    }
   // sprawdzamy czy jest polnoc
   if ((hour == 0 || hour == 24) && (minute == 0 || minute == 60) && (second ==00 || second == 60)) {
-	  if(GPIO_ReadInputPin(GPIOD,GPIO_PIN_4) == RESET) { //Jesli niski stan PWM
-	    GPIO_WriteHigh(GPIOC, GPIO_PIN_1); //wylacz buzzer
+	  if(GPIO_ReadInputPin(GPIOD,GPIO_PIN_4) == RESET) { 
+	    GPIO_WriteHigh(GPIOC, GPIO_PIN_1); 
    }
 	  else {
-	   GPIO_WriteLow(GPIOC, GPIO_PIN_1); //wlacz buzzer
+	   GPIO_WriteLow(GPIOC, GPIO_PIN_1); 
 	  }
 	}
-		if (manual_clock_state != 1) {
-        current_display_state = manual_clock_state; // Wymuszenie guzikiem
+	if (manual_clock_state != 1) {
+        current_display_state = manual_clock_state; 
     } else {
-        current_display_state = clock_state; // Normalna rotacja
+        current_display_state = clock_state; 
     }
 	switch(current_display_state){
 		case 1:
@@ -306,10 +300,11 @@ void main(void)
 		
 	}
 	refreshSegm();
-		if(tick_ms >= 1000 && zegar_zyje == true){
-			tick_ms = 0;
-			second++;
-		//Magda: Ustawianie sekwencji diód (Dopasowane do sterowania stanem NISKIM!)
+	if(tick_ms >= 1000 && zegar_zyje == true){
+		tick_ms = 0;
+		second++;
+		
+		//Magda: Ustawianie sekwencji diód
 		if (krok_diod == 0) {       // Krok 1: Tylko D1 (Low = ON, High = OFF)
 			GPIO_WriteLow(GPIOC, GPIO_PIN_5);
 			GPIO_WriteHigh(GPIOC, GPIO_PIN_7);
@@ -358,66 +353,49 @@ void main(void)
 		if (krok_diod >= 7) {
 			krok_diod = 0;
 		}
-		
-			if(second%3 == 0){
-				clock_state = (clock_state+1)%3;
-			}
-			if (manual_clock_state != 0) {
-        current_display_state = manual_clock_state; // Wymuszenie guzikiem
-			} else {
-        current_display_state = clock_state; // Normalna rotacja
-			}
-			if(second >=60){
-				second = 0;
-				minute++;
-				if(minute >= 60){
-					minute = 0;
-					hour++;
-					
-					if(hour >= 24){
-						day++;
-						hour = 0;
-						if(day > month[current_month-1]){
-							current_month++;
-							day = 1;
-							if(current_month >=13){
+
+		if(second%3 == 0){
+			clock_state = (clock_state+1)%3;
+		}
+		if (manual_clock_state != 0) {
+			current_display_state = manual_clock_state; 
+		} else {
+			current_display_state = clock_state; 
+		}
+		if(second >=60){
+			second = 0;
+			minute++;
+			if(minute >= 60){
+				minute = 0;
+				hour++;
+				
+				if(hour >= 24){
+					day++;
+					hour = 0;
+					if(day > month[current_month-1]){
+						current_month++;
+						day = 1;
+						if(current_month >=13){
 							current_month = 1;
-							}
 						}
 					}
 				}
 			}
-			convertNumber(second,seconds);
-			convertNumber(minute,minutes);
-			convertNumber(hour, hours);
-			convertNumber(day, days);
-			convertNumber(current_month, months);
 		}
+		convertNumber(second,seconds);
+		convertNumber(minute,minutes);
+		convertNumber(hour, hours);
+		convertNumber(day, days);
+		convertNumber(current_month, months);
+	}
  }
 }
 
-
-
 #ifdef USE_FULL_ASSERT
-
-/**
- * @brief Reports the name of the source file and the source line number
- *  where the assert_param error has occurred.
- * @param file: pointer to the source file name
- * @param line: assert_param error line source number
- * @retval : None
- */
 void assert_failed(u8* file, u32 line)
 { 
- /* User can add his own implementation to report the file name and line number,
-   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
- /* Infinite loop */
  while (1)
  {
  }
 }
 #endif
-
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
